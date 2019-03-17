d7269376134204357f80d68c86e97c2f57d6bda8
Lukasz Patalas <dev@lukaszpatalas.pl>
2019-03-17 20:55:20 +0100

    Add keyDown handler

--- src/app.fs
+++ src/app.fs
@@ -21,19 +21,48 @@ let drawShape (x: int) (y:int) (color: string) (Shape coords) (canvas: HTMLCanva
     coords
     |> Seq.iter (fun ({ x = cx; y = cy }) -> drawSquare (x + cx) (y + cy) color canvas)
 
+type MoveDirection = Left | Right | Up | Down
+
 let run containerDivId =
     let elem = document.getElementById containerDivId
     let canvas = document.createElement_canvas()
     canvas.width <- float (fst gameAreaSize) * blockSize
     canvas.height <- float (snd gameAreaSize) * blockSize
     elem.appendChild(canvas) |> ignore
-    clearCanvas canvas
-    drawShape 0 0  "red" (createShape D) canvas
-    drawShape 0 2  "green" (createShape I) canvas
-    drawShape 0 5  "yellow" (createShape J) canvas
-    drawShape 0 8  "cyan" (createShape L) canvas
-    drawShape 0 11  "orange" (createShape O) canvas
-    drawShape 0 14  "magenta" (createShape S) canvas
-    drawShape 0 17 "blue" (createShape Z) canvas
+
+    let shape = createShape D
+    let mutable pos = { x = 0; y = 0 }
+
+    let redraw() =
+        clearCanvas canvas
+        drawShape pos.x pos.y "red" shape canvas
+
+    let moveBlock (direction: MoveDirection) =
+        let newPos =
+            match direction with
+            | Left -> { pos with x = pos.x - 1 }
+            | Right -> { pos with x = pos.x + 1 }
+            | Up -> { pos with y = pos.y - 1 }
+            | Down -> { pos with y = pos.y + 1 }
+        pos <- newPos
+
+    let onKeyDown (e: KeyboardEvent) =
+        let moveDirection =
+            match e.key with
+            | "ArrowLeft" -> Some Left
+            | "ArrowRight" -> Some Right
+            | "ArrowUp" -> Some Up
+            | "ArrowDown" -> Some Down
+            | _ -> None
+
+        match moveDirection with
+        | Some dir ->
+            moveBlock dir
+            redraw()
+        | None -> ()
+
+    window.addEventListener_keydown onKeyDown
+
+    redraw()
 
 run "mainContainer"
